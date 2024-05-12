2894345cb0be9847a8bc23e7b1e20c378d27aa70
maslog <ronaldmaslog5@gmail.com>
2024-05-12 10:04:09 +0800

    First Design For the Chick-INT

---
+++ src/main/java/com/mycompany/chick_int_dtr_system/Chick_Int_DTR_System.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.chick_int_dtr_system;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author Ronald
+ */
+public class Chick_Int_DTR_System {
+
+    public static void main(String[] args) {
+        FlatLightLaf.setup();
+        UIManager.put("jButton2.arc", 999);
+        
+        new UserUI().setVisible(true);
+        
+    }
+}
---
+++ src/main/java/com/mycompany/chick_int_dtr_system/Components/DigitalClock.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chick_int_dtr_system.Components;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Ronald
+ */
+public class DigitalClock {
+
+    public String updateTime() {
+
+        //Get the current time and format it
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat timeFormatter = new SimpleDateFormat("hh:mm:ss a");
+        String time = timeFormatter.format(calendar.getTime());
+
+        return time;
+
+    }
+
+    public String updateDate() {
+        //Get the current date and format it
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("EE, MMM dd, yyyy");
+        String date = dateFormatter.format(calendar.getTime());
+
+        return date;
+    }
+
+}
